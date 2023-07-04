031f9b1ad46eadd3fee4ff1a4aa2ee571ec6de40
wyslan alves <wyslandasilvaalves@gmail.com>
2023-07-04 17:13:20 -0300

    Dia 1 do desafio de exercícios Start by Capgemini

---
+++ src/exercicio01/Exercicio1.java
@@ -0,0 +1,31 @@
+package exercicio01;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+    /*
+                                            Lista De Exercícios
+       Objetivo do Exercício : Faça um algoritmo que receba dois números e exiba o resultado da sua soma
+
+    */
+        int numero1;
+        int numero2;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu Primeiro Número:");
+        numero1 = sc.nextInt();
+
+        System.out.println("Digite seu Segundo Número:");
+        numero2 = sc.nextInt();
+
+        int resultado = numero1 + numero2;
+
+        System.out.println("Resultado : " + resultado);
+
+
+
+
+
+    }
+}
